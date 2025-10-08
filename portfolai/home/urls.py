8c37e1c2a699cc7d03419f71e1214af2821293df
Elizandro I Duran <188512019+eduran04@users.noreply.github.com>
2025-10-08 01:30:04 -0600

    Remove JS files

--- portfolai/home/urls.py
+++ portfolai/home/urls.py
@@ -4,7 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('api/market-data/', views.api_market_data, name='market_data'),
-    path('api/chart-data/', views.api_chart_data, name='chart_data'),
-    path('api/portfolio-stats/', views.api_portfolio_stats, name='portfolio_stats'),
 ]
